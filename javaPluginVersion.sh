182be5fc46c436e6fcbee6e55b816a0f5d9aa604
JAMF Software <bram.cohen@jamfsoftware.com>
2013-05-16 12:21:54 -0400

    Update javaPluginVersion.sh

--- javaPluginVersion.sh
+++ javaPluginVersion.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
-jver=$(/bin/cat /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Info.plist | grep 'jpi-version' | tr '</key>','=' ' '| awk '{print $NF}')
+# Check to see if Java 1.6 is installed
+if [[ -e /Library/Java/Home ]]; then
+echo "<result>$(java -version 2>&1 | awk '/version/{print $3}' | sed 's/"//g')</result>"
 
-echo "<result>$jver</result>"
+# Check to see if Java 1.7 is installed
+elif [[ -e /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin ]]; then
+echo "<result>$(cat /Library/Internet\ Plug-Ins/JavaAppletPlugin.plugin/Contents/Info.plist | grep 'jpi-version' | tr '=','<' ' ' | awk '{print $2}')</result>"
+
+# Report back if Java is not found
+else
+echo "<result>Not Installed</result>"
+fi
